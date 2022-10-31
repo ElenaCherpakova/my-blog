292fa9fa82cf7778c01af9b80a80a6daeb088f51
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-10-30 22:40:56 -0400

    setup project and add schema, create components

---
+++ REACT3.0/my-blog/components/BuyMeCoffee/index.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.module.css";
+
+const BuyMeCoffee = () => {
+  return <div>index</div>;
+};
+
+
+export default BuyMeCoffee;
---
+++ REACT3.0/my-blog/components/Section/index.jsx
@@ -0,0 +1,8 @@
+import './index.module.scss';
+
+const Section= () => {
+  return <div>index</div>;
+};
+
+
+export default Section;
