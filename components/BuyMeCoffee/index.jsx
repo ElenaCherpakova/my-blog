0f9375e0058b4b7a3b9d561fa2809b4ff79b754d
ElenaCherpakova <elena.cherpakova@gmail.com>
2022-11-01 21:57:29 -0400

    add generated stripe link

--- components/BuyMeCoffee/index.jsx
+++ components/BuyMeCoffee/index.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 import styles from './index.module.scss';
+
 import cl from 'classnames';
 const BuyMeCoffee = ({ className }) => {
-  return <div>index</div>;
+  return <div className={(cl(className, styles.buyCoffee))}>
+    <a href="https://buy.stripe.com/test_bIY16d8a9emaaB24gg"
+      target="_blank"
+      className={styles.buyCoffeeButton}
+      rel="noreferrer"
+    >Buy me a Coffee</a>
+  </div>;
 };
+
+
 
 
 export default BuyMeCoffee;
